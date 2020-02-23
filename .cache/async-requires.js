db1aafdfe0a5206793700f1402f22ffff8a0f0b6
Riomar Mccartney <riomarmccartney@gmail.com>
2020-02-23 16:09:05 +0900

    Remove git ignored dir

--- .cache/async-requires.js
+++
@@ -1,7 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-pages-index-jsx": () => import("../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */)
-}
-
