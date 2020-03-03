a922240daecf6e8fdb446d84037e13fb44ac8072
Riomar Mccartney <riomarmccartney@gmail.com>
2020-03-03 17:25:15 +0900

    Update profile text

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,14 +5,17 @@ import Nav from "../components/nav"
 const IndexPage = () => {
     return (
         <Layout>
-          <p className="font-body">Riomar Mccartney / マッカートニー龍馬 is a British Japanese
-product designer & visual artist based in Tokyo. 
-Working at the intersection of expression, interaction design, and engineering with an interest in media arts and graphic design.
-
-He’s currently Head of Design at ookami, bringing Player!, a social community platform bonding local school / amateur sports teams, brands and fan communities together across Japan. 
-On the side, he’s a designer in crime at NobodySurf, where you can binge-watch free-surfing videos created by surfers and filmers from around the world.
-
-For further information, please check the résumé → </p>
+            <navigationException className="md:absolute"><Nav /></navigationException>
+            <section className="pt-0 md:pt-4 pb-4 grid col-gap-2 md:col-gap-4 row-gap-8 md:row-gap-4 grid-cols-12">
+                <intro className="col-start-1 col-end-13 md:col-end-6">
+                    Riomar McCartney is a British Japanese designer & visual artist based in Tokyo, Japan. 
+                    His work focuses on meeting expression, interaction design, and engineering together through product design, graphic communications, and media arts. 
+                </intro>
+                <intro className="col-start-1 col-end-13 md:col-end-6">
+                    He is Co-Founder of upcoming.studio, a design studio founded with his partner in crime, Cyrus Korn. Before co-founding upcoming.studio, Riomar worked as Chief Design Officer for ookami.tokyo, designing Player!, and worked independently as a product designer for various creative agencies as well as technology companies, with work recognized by Forbes Japan, Good Design Awards and Apple App Store.
+                </intro>
+            </section>
+            
         </Layout>
     )
 }
