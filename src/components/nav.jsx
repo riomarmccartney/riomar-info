0be88872dfb44fdd0b3e3f7badd896b6a158ded5
Riomar Mccartney <riomarmccartney@gmail.com>
2020-03-04 17:09:03 +0900

    Remove Vis Bank for now

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -13,24 +13,23 @@ const Nav = () => {
             style: 'col-start-1 md:col-start-1 row-start-1',
             id: 1
         },
-        {
+        /* {
             title: 'Vis Bank',
             betweenPages: true,
             path: '/visualbank/',
             style: 'col-start-10 md:col-start-11 row-start-1',
             id: 2
-        },
+        }, */
         {
             title: 'Contact',
             betweenPages: false,
             path: 'mailto:contact@riomar.co?subject=Question%20about%20your%20work%20/%20Quote%20request%20/%20Work%20proposal&body=Hi%20Riomar,%0D%0A%0D%0A',
-            style: 'col-start-10 md:col-start-11 row-start-2',
+            style: 'hover:opacity-50 col-start-10 md:col-start-11 row-start-1',
             id: 3
         }
     ]);
-
     return (
-        <nav className="py-4 grid grid-cols-12 grid-rows-2 col-gap-2 md:col-gap-4 whitespace-no-wrap">
+        <nav className="py-6 grid grid-cols-12 grid-rows-2 col-gap-2 md:col-gap-4 whitespace-no-wrap">
             <span className="col-start-7 md:col-start-8 row-start-1"><Clock format={'HH:mm'} timezone={'Asia/Tokyo'} /><span style={{fontSize: '9px'}}> [東京] </span></span>
             {links.map(link => {
                 if (link.betweenPages) {
@@ -39,7 +38,6 @@ const Nav = () => {
                     return ( <a key={link.id} href={link.path} className={link.style}>{link.title}</a> )
                 }
             })}
-            
         </nav>
     )
 }
