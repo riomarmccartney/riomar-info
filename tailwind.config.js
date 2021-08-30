3e1057c234abd102b35c41e08ff7620014ccd5b5
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-08-30 17:47:24 +0900

    Change style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      typeface: ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
