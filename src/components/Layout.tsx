eecf42f6997504e4b5ee6b62de32a741f9611f46
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 13:46:47 +0900

    Fix spaces

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react"
-import { Sidebar } from "./Sidebar"
+import { ReactNode } from 'react'
+import { Sidebar } from './Sidebar'
 
 type LayoutType = {
   children: ReactNode,
@@ -9,9 +9,9 @@ type LayoutType = {
 export const Layout = ({ children }: LayoutType) => {
   return (
     <div className="flex flex-row w-full h-screen text-base divide-x divide-black font-regular font-typeface tracking-base">
-      <Sidebar className="w-1/5 py-8 pl-12 space-y-24" />
+      <Sidebar className="w-1/5 max-w-sm py-8 pl-8 pr-16 space-y-24 min-w-min whitespace-nowrap" />
 
-      <div className="flex-1 px-12 py-8 overflow-y-scroll">
+      <div className="flex-1 py-8 pl-8 pr-24 overflow-y-scroll">
         <div className="space-y-24 max-w-7xl">
           {children}
         </div>
