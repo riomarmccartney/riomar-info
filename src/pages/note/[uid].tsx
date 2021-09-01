482d3f742c6c12b1f21edcfbba751e00ed8d9ab8
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 16:30:28 +0900

    Import changes to uid page

--- src/pages/note/[uid].tsx
+++ src/pages/note/[uid].tsx
@@ -1,23 +1,29 @@
 import Prismic from '@prismicio/client'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import SliceZone from 'next-slicezone'
+import resolver from 'utils/sliceMachineResolver'
 import { Note } from 'src/components/Note'
 import Client from 'utils/prismicHelpers'
 import { RichText } from 'prismic-reactjs'
 import { htmlSerializer } from 'utils/prismicRichTextSerializer'
+import { Layout } from 'src/components/Layout'
 
 type ParameterType = {
   uid: string
 }
 
-export default function NotePage({ note }) {
+export default function NotePage({ note }: any) {
   return (
-    <Note 
-      uid={note.uid}
-      title={RichText.asText(note.data.title)}
-      date={note.first_publication_date}
-      article={<RichText render={note.data.article} htmlSerializer={htmlSerializer} />}
-      caption={<RichText render={note.data.caption} htmlSerializer={htmlSerializer} />}
-    />
+    <Layout>
+      <Note 
+        key={note.uid}
+        uid={note.uid}
+        title={RichText.asText(note.data.title)}
+        date={note.first_publication_date}
+        article={<SliceZone resolver={resolver} slices={note.data.body}/>}
+        caption={<RichText render={note.data.caption} htmlSerializer={htmlSerializer} />}
+      />
+    </Layout>
   )
   
 }
