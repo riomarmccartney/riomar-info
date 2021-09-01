3558645cf4e8564bada07ec120e68b230c3fff63
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 15:17:02 +0900

    Decalre missing type for next-slicezone

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,5 +2,9 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
+declare module 'next-slicezone'
+declare module 'next-slicezone/hooks'
+declare module 'next-slicezone/resolver'
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
