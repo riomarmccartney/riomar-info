5da904c7fe12157f87ad70fc10f12e5165fd2ad9
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 19:23:50 +0900

    Refine utility

--- utils/dateFormatter.ts
+++ utils/dateFormatter.ts
@@ -5,9 +5,4 @@ import timezone from 'dayjs/plugin/timezone'
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-type dateFormatterType = {
-  date: string,
-  format?: string,
-}
-
-export const dateFormatter = ({ date, format }: dateFormatterType) => dayjs(date).tz('Asia/Tokyo').format(format || 'YYYY-MM-DD HH:mm')
+export const dateFormatter = (date: string) => dayjs(date).tz('Asia/Tokyo').format('YYYY-MM-DD HH:mm')
