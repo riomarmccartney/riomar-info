e331bf2a94e1555cce6c18c8ef9b6f728d44323c
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 14:08:22 +0900

    Add type

--- src/components/slices/MultiStory.tsx
+++ src/components/slices/MultiStory.tsx
@@ -1,9 +1,9 @@
 import { RichText } from 'prismic-reactjs'
 import { htmlSerializer } from 'utils/prismicRichTextSerializer'
 
-export const MultiStory = ({ slice }) => (
+export const MultiStory = ({ slice }: { slice: any }) => (
   <div className="flex flex-row space-x-12">
-    {slice.items.map(({ paragraph }, i) => (
+    {slice.items?.map(({ paragraph }: any, i: number) => (
       <div key={i} className="flex-1 max-w-2/5"><RichText render={paragraph} htmlSerializer={htmlSerializer} /></div>
     ))}
   </div>
