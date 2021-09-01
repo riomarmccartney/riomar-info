b42fec26c90e1731f948274e5624ca19b6cc1c10
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 19:56:29 +0900

    Update layout

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,16 +1,14 @@
-import { molecularSpacing, subAtomicSpacing } from 'src/constants/spacing'
+import { molecularSpacing, atomicSpacing } from 'src/constants/spacing'
 import { NoteType } from 'src/types/note'
 import { dateFormatter } from 'utils/dateFormatter'
-
 
 export const Note = ({title, article, caption, date, uid}: NoteType) => {
   return (
     <article id={uid} className={(molecularSpacing)}>
-      <div className={(subAtomicSpacing)}>
+      <div className={(atomicSpacing)}>
         <span>{dateFormatter(date)}</span>
-        <h2 >{title}</h2>
+        <h2>{title}</h2>
       </div>
-      
       {article}
       <div className='h-px border-t border-black border-dashed opacity-25'></div>
       <div className='text-sm opacity-25'>{caption}</div>
--- src/components/slices/MultiStory.tsx
+++ src/components/slices/MultiStory.tsx
@@ -1,12 +1,12 @@
 import clsx from 'clsx'
 import { RichText } from 'prismic-reactjs'
-import { subAtomicSpacing } from 'src/constants/spacing'
+import { atomicSpacing } from 'src/constants/spacing'
 import { htmlSerializer } from 'utils/prismicRichTextSerializer'
 
 export const MultiStory = ({ slice }: { slice: any }) => (
   <div className='flex flex-row space-x-12'>
     {slice.items?.map(({ paragraph }: any, i: number) => (
-      <div key={i} className={clsx('flex-1 w-1/2', subAtomicSpacing)}><RichText render={paragraph} htmlSerializer={htmlSerializer} /></div>
+      <div key={i} className={clsx('flex-1 w-1/2', atomicSpacing)}><RichText render={paragraph} htmlSerializer={htmlSerializer} /></div>
     ))}
   </div>
 )
