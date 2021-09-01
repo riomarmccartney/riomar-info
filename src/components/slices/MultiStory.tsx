e9d9da314af10cd838754ef8d91ec32a642e2d7a
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 19:40:10 +0900

    Fix layout

--- src/components/slices/MultiStory.tsx
+++ src/components/slices/MultiStory.tsx
@@ -1,12 +1,12 @@
 import clsx from 'clsx'
 import { RichText } from 'prismic-reactjs'
-import { subAtomicSpacig } from 'src/constants/spacing'
+import { subAtomicSpacing } from 'src/constants/spacing'
 import { htmlSerializer } from 'utils/prismicRichTextSerializer'
 
 export const MultiStory = ({ slice }: { slice: any }) => (
   <div className='flex flex-row space-x-12'>
     {slice.items?.map(({ paragraph }: any, i: number) => (
-      <div key={i} className={clsx('flex-1 w-1/2', subAtomicSpacig)}><RichText render={paragraph} htmlSerializer={htmlSerializer} /></div>
+      <div key={i} className={clsx('flex-1 w-1/2', subAtomicSpacing)}><RichText render={paragraph} htmlSerializer={htmlSerializer} /></div>
     ))}
   </div>
 )
