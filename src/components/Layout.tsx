33c97d86568f97a7f349d08f5229649bac088878
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 20:56:54 +0900

    Move to Index

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,4 @@
 import { ReactNode } from 'react'
-import { Sidebar } from './Sidebar'
-import clsx from 'clsx'
-import { elementSpacing } from 'src/constants/spacing'
 
 type LayoutType = {
   children: ReactNode,
@@ -11,12 +8,7 @@ type LayoutType = {
 export const Layout = ({ children }: LayoutType) => {
   return (
     <div className='flex flex-row h-screen text-base bg-gray-100 divide-x divide-black font-regular font-typeface tracking-base'>
-      <Sidebar className={clsx('w-1/5 py-8 pl-8 pr-24 min-w-min whitespace-nowrap max-w-sm', elementSpacing)}/>
-      <main className='flex-1 w-full px-16 py-8 overflow-y-scroll'>
-        <div className={clsx('max-w-7xl mx-auto', elementSpacing)}>
-          {children}
-        </div>
-      </main>
+      {children}
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,23 +8,32 @@ import { RichText } from 'prismic-reactjs'
 import { htmlSerializer } from 'utils/prismicRichTextSerializer'
 import { Layout } from 'src/components/Layout'
 import { Clock } from 'src/components/UI/Clock'
+import { Sidebar } from 'src/components/Sidebar'
+import { elementSpacing } from 'src/constants/spacing'
+import clsx from 'clsx'
 
 export default function Index({ notes }: { notes: any }) {
   return (
     <Layout>
-      <Clock />
-      {notes.map((note: any) => {
-        return (
-          <Note 
-            key={note.uid}
-            uid={note.uid}
-            title={RichText.asText(note.data.title)}
-            date={note.first_publication_date}
-            article={<SliceZone resolver={resolver} slices={note.data.body}/>}
-            caption={<RichText render={note.data.caption} htmlSerializer={htmlSerializer} />}
-          />
-        )}
-      )}
+      <Sidebar notes={notes} className={clsx('w-1/5 py-8 pl-8 pr-24 min-w-min whitespace-nowrap max-w-sm', elementSpacing)}/>
+      <main className='flex-1 w-full px-16 py-8 overflow-y-scroll'>
+        <div className={clsx('max-w-7xl mx-auto', elementSpacing)}>
+          <Clock />
+          {notes.map((note: any) => {
+            return (
+              <Note 
+                key={note.uid}
+                uid={note.uid}
+                title={RichText.asText(note.data.title)}
+                date={note.first_publication_date}
+                article={<SliceZone resolver={resolver} slices={note.data.body}/>}
+                caption={<RichText render={note.data.caption} htmlSerializer={htmlSerializer} />}
+              />
+            )}
+          )}
+        </div>
+      </main>
+      
     </Layout>
   )
 }
