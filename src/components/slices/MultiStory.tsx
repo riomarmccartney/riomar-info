70ee56b211f2f9b9a6e97b72297276c014743b1d
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 13:10:21 +0900

    Fix key

--- src/components/slices/MultiStory.tsx
+++ src/components/slices/MultiStory.tsx
@@ -1,12 +1,11 @@
 import { RichText } from 'prismic-reactjs'
 import { htmlSerializer } from 'utils/prismicRichTextSerializer'
 
-export const MultiStory = ({ slice }) => { 
-  return (
-    <div className="flex flex-row space-x-12">
-      {slice.items.map(({ paragraph }) => (
-        <div className="flex-1 max-w-2/5"><RichText render={paragraph} htmlSerializer={htmlSerializer} /></div>
-      ))}
-    </div>
-  )
-}
+export const MultiStory = ({ slice }) => (
+  <div className="flex flex-row space-x-12">
+    {slice.items.map(({ paragraph }, i) => (
+      <div key={i} className="flex-1 max-w-2/5"><RichText render={paragraph} htmlSerializer={htmlSerializer} /></div>
+    ))}
+  </div>
+)
+
