eb1eb60c635d8471870d6d7d2d1e07967ec7d952
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-01 14:08:32 +0900

    Add type

--- src/components/slices/SingleStory.tsx
+++ src/components/slices/SingleStory.tsx
@@ -1,10 +1,12 @@
 import { RichText } from 'prismic-reactjs'
 import { htmlSerializer } from 'utils/prismicRichTextSerializer'
 
-export const SingleStory = ({ slice }) => {
+export const SingleStory = ({ slice }: { slice: any }) => {
+  const paragraph = slice.primary?.paragraph
+  
   return (
     <div className="flex-1 w-1/2">
-      <RichText render={slice.primary.paragraph} htmlSerializer={htmlSerializer} />
+      <RichText render={paragraph} htmlSerializer={htmlSerializer} />
     </div>
   )
 }
