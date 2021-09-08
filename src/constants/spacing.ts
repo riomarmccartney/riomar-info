6b313095933d249dc7da332fb1d4f48e58deb688
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-08 16:58:50 +0900

    Add new spacings

--- src/constants/spacing.ts
+++ src/constants/spacing.ts
@@ -1,5 +1,7 @@
-const elementSpacing = 'space-y-36'
-const molecularSpacing = 'space-y-12'
-const atomicSpacing = 'space-y-2'
+const universeSpacing = 'space-y-36'
+const elementSpacing = 'space-y-16'
+const molecularSpacing = 'space-y-8'
+const atomicSpacing = 'space-y-4'
+const subAtomicSpacing = 'space-y-2'
 
-export { elementSpacing, molecularSpacing, atomicSpacing }
+export { universeSpacing, elementSpacing, molecularSpacing, atomicSpacing, subAtomicSpacing }
