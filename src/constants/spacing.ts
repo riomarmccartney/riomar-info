0ffa10344cb2ce052df74ffad5734aa0b5dd207b
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-11 00:53:17 +0900

    Refactor spacing

--- src/constants/spacing.ts
+++
@@ -1,7 +0,0 @@
-const universeSpacing = 'space-y-36'
-const elementSpacing = 'space-y-16'
-const molecularSpacing = 'space-y-8'
-const atomicSpacing = 'space-y-6'
-const subAtomicSpacing = 'space-y-2'
-
-export { universeSpacing, elementSpacing, molecularSpacing, atomicSpacing, subAtomicSpacing }
