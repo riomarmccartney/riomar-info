224705890587e50cfe4073f22d0f36c707b24b33
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-15 18:42:45 +0900

    Improve to dynamic link

--- src/components/UI/NextLink.tsx
+++ src/components/UI/NextLink.tsx
@@ -3,18 +3,29 @@ import { ReactNode } from 'react'
 
 type NextLinkType = {
   children: ReactNode,
-  href: string,
+  onClick?: ReactNode,
+  href?: string,
+
 }
 
-export const NextLink = ({ children, href, ...rest }: NextLinkType) => {
-  return (
-    <Link 
-      href={href} 
-      passHref
-      {...rest}>
-      <a className='transition-all duration-150 bg-transparent border-b border-gray-400 border-solid hover:bg-black hover:text-white hover:border-black'>
-        {children}
-      </a>
-    </Link>
-  )
+ 
+
+export const NextLink = ({ children, href, onClick, ...rest }: NextLinkType) => {
+  const classname = 'transition-all duration-150 bg-transparent border-b border-gray-400 border-solid cursor-pointer hover:bg-black hover:text-white hover:border-black'
+
+  switch (href) {
+  case '' || undefined:
+    return <span className={classname} {...onClick}>{children}</span>
+
+  default:
+    return (
+      <Link 
+        href={href} 
+        passHref
+        {...rest}
+        {...onClick}>
+        <a className={classname}>{children}</a>
+      </Link>
+    )
+  }
 }
