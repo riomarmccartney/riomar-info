725d986e7343a8bdf38186a281e39c2c6d94e8e8
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-15 19:19:50 +0900

    Refactor Sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,11 +10,11 @@ type LayoutType = {
 export const Layout = ({ children, notes }: LayoutType) => {
   return (
     <div className='flex flex-col text-base md:h-screen md:space-y-0 md:flex-row font-regular font-typeface tracking-base'>
-      <nav className="fixed z-50 w-full px-4 mt-4 text-gray-100 border-black border-solid md:overflow-scroll md:text-current md:relative md:mt-0 lg:pt-8 md:py-8 md:pl-8 md:pr-16 md:border-r min-w-min md:w-1/6 mix-blend-difference md:mix-blend-normal">
-        <div className="max-w-2xl mx-auto md:flex md:flex-col md:mx-0 md:max-w-sm md:justify-between md:h-full">
-          <Sidebar notes={notes} className='box-border py-2 border-b border-gray-100 border-solid md:py-0 md:border-none '/>
-        </div>
-      </nav>
+      <Sidebar 
+        notes={notes} 
+        wrapperClassName='fixed z-50 w-full px-4 mt-2 md:mt-0 lg:pt-8 md:py-8 md:pl-8 md:pr-16 min-w-min md:w-1/6'
+        className='max-w-2xl mx-auto md:mx-0 md:max-w-sm'
+      />
       <main className='flex-1 w-full px-4 mt-40 md:mt-0 md:overflow-y-scroll md:py-8 md:px-16'>
         <div className="max-w-2xl pb-12 mx-auto md:mx-0 md:max-w-7xl md:pb-18">
           <div className='space-y-universal'>{children}</div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -20,43 +20,46 @@ type NoteType = {
 
 type SidebarType = {
   className?: string,
+  wrapperClassName?: string,
   notes: any,
 }
 
-export const Sidebar = ({ className, notes }: SidebarType) => {
+export const Sidebar = ({ className, wrapperClassName, notes }: SidebarType) => {
   const tags = ['Notes', 'Work']
 
   return (
-    <>
-      <div className={clsx(className, 'whitespace-nowrap space-y-universal')}>
-        <div className="flex flex-row items-center justify-between">
-          <div className="w-1/4 md:my-1 md:w-auto">
-            <Link href='/' passHref>
-              <a className="transition-colors duration-150 bg-transparent md:leading-none md:table-cell md:text-center md:align-middle md:border md:border-black md:rounded-full md:w-10 md:h-10 md:boder-solid hover:text-white hover:bg-black">
+    <nav className={clsx(wrapperClassName, 'text-gray-100 border-black border-solid md:overflow-scroll md:text-current md:relative md:border-r mix-blend-difference md:mix-blend-normal')}>
+      <div className={clsx(className)}>
+        <div className='box-border py-2 border-b border-gray-100 border-solid md:py-0 md:border-none whitespace-nowrap space-y-universal md:justify-between md:h-full md:flex md:flex-col '>
+          <div className="flex flex-row items-center justify-between">
+            <div>
+              <Link href='/' passHref>
+                <a className="transition-colors duration-150 bg-transparent md:leading-none md:table-cell md:text-center md:align-middle md:border md:border-black md:rounded-full md:w-10 md:h-10 md:boder-solid hover:text-white hover:bg-black">
             RM
-              </a>
-            </Link>
+                </a>
+              </Link>
+            </div>
+          
+            <div>
+              <span className="transition-colors duration-150 bg-transparent md:hidden hover:text-white hover:bg-black">Index</span>
+            </div>
           </div>
-          <div className="md:hidden"><Clock /></div>
-          <div className="w-1/4 text-right md:w-auto">
-            <span className="transition-colors duration-150 bg-transparent md:hidden hover:text-white hover:bg-black">Index</span>
-          </div>
-        </div>
 
-        <div className='hidden md:block space-y-molecular '>
-          {tags.map((tag, i) => {
-            return (
-              <TagList 
-                key={i}
-                title={tag}
-                content={notes}
-              />
-            )
-          })}
+          <div className='hidden md:block space-y-molecular '>
+            {tags.map((tag, i) => {
+              return (
+                <TagList 
+                  key={i}
+                  title={tag}
+                  content={notes}
+                />
+              )
+            })}
+          </div>
         </div>
+        <div className="hidden mt-36 md:block"><Clock /></div>
       </div>
-      <div className="hidden mt-36 md:block"><Clock /></div>
-    </>
+    </nav>
   )
 }
 
