1f48cfd2f25b3918d8304ade335f6b1ad5fa3592
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-20 00:50:06 +0900

    Update layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export const Layout = ({ children, notes }: LayoutType) => {
     <div className='flex flex-col text-base md:h-screen md:space-y-0 md:flex-row font-regular font-typeface tracking-base'>
       <Sidebar 
         notes={notes} 
-        wrapperClassName='fixed z-50 w-full px-4 mt-2 md:mt-0 lg:pt-8 md:py-8 md:pl-8 md:pr-16 min-w-min md:w-1/6'
+        wrapperClassName='fixed z-50 w-full px-4 pt-2 lg:pt-8 md:py-8 md:pl-8 md:pr-16 min-w-min md:w-1/6'
         className='max-w-2xl mx-auto md:mx-0 md:max-w-sm'
       />
       <main className='flex-1 w-full px-4 mt-40 md:mt-0 md:overflow-y-scroll md:py-8 md:px-16'>
