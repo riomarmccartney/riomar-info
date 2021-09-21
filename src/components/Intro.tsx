34d0cca8516e3b29195b01d99df69b5519ab2356
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-21 18:19:04 +0900

    Place image for mobile

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -12,20 +12,8 @@ export const Intro = ({ content }: {content: any}) => (
     </div>
     <Note 
       article={
-        <MultiStory>
-          <div className='lg:w-1/2 space-y-molecular'>
-            <div className='space-y-atomic'><RichText render={content.data.intro} htmlSerializer={htmlSerializer} /></div>
-            <ul className="flex flex-row space-x-6">{content.data.links.map(({ link }: { link: any}) => {
-              return (
-                <li key={RichText.asText(link)}>
-                  <span className="inline-block"><RichText render={link} htmlSerializer={htmlSerializer} /></span>
-                  <sup className="ml-0.5 ">↗</sup>
-                </li>
-              )
-            })}
-            </ul>
-          </div>
-          <div className="ml-6 lg:w-1/2">
+        <>
+          <div className="lg:hidden">
             <NextImage 
               src={content.data.profile_picture.url}
               alt={content.data.profile_picture.alt}
@@ -33,7 +21,29 @@ export const Intro = ({ content }: {content: any}) => (
               width={content.data.profile_picture.dimensions.width}
             />
           </div>
-        </MultiStory>
+          <MultiStory>
+            <div className='lg:w-1/2 space-y-molecular'>
+              <div className='whitespace-pre-wrap space-y-atomic'><RichText render={content.data.intro} htmlSerializer={htmlSerializer} /></div>
+              <ul className="flex flex-row space-x-6">{content.data.links.map(({ link }: { link: any}) => {
+                return (
+                  <li key={RichText.asText(link)}>
+                    <span className="inline-block"><RichText render={link} htmlSerializer={htmlSerializer} /></span>
+                    <sup className="ml-0.5">↗︎</sup>
+                  </li>
+                )
+              })}
+              </ul>
+            </div>
+            <div className="hidden lg:block lg:w-1/2">
+              <NextImage 
+                src={content.data.profile_picture.url}
+                alt={content.data.profile_picture.alt}
+                height={content.data.profile_picture.dimensions.height}
+                width={content.data.profile_picture.dimensions.width}
+              />
+            </div>
+          </MultiStory>
+        </>
       }
       caption={<RichText render={content.data.colophon} htmlSerializer={htmlSerializer} />}
     />
