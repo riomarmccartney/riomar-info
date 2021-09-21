b0c67f192b3b3b7c91eb5d713725f8420ebb0ec1
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-21 15:39:55 +0900

    Remove onclick from Link

--- src/components/UI/Hyperlink.tsx
+++ src/components/UI/Hyperlink.tsx
@@ -3,28 +3,24 @@ import { ReactNode } from 'react'
 
 type HyperlinkType = {
   children: ReactNode,
-  onClick?: ReactNode,
   href?: string,
 
 }
 
- 
-
-export const Hyperlink = ({ children, href, onClick, ...rest }: HyperlinkType) => {
-  const classname = 'transition-all duration-150 bg-transparent border-b border-gray-400 border-solid cursor-pointer hover:bg-black hover:text-white hover:border-black'
+export const Hyperlink = ({ children, href, ...rest }: HyperlinkType) => {
+  const className = {className: 'transition-all duration-150 bg-transparent border-b border-gray-400 border-solid cursor-pointer hover:bg-black hover:text-white hover:border-black'}
 
   switch (href) {
   case '' || undefined:
-    return <span className={classname} {...onClick}>{children}</span>
+    return <span {...className}>{children}</span>
 
   default:
     return (
       <Link 
-        href={href} 
-        passHref
+        href={href}
         {...rest}
-        {...onClick}>
-        <a className={classname}>{children}</a>
+      >
+        <a {...className}>{children}</a>
       </Link>
     )
   }
