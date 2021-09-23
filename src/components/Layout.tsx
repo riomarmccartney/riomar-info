5561a74c0792806678f5687c63f53a3761a47c03
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-24 00:32:00 +0900

    Fix margin

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,7 +15,7 @@ export const Layout = ({ children, notes }: LayoutType) => {
         wrapperClassName='fixed w-full z-50 px-4 py-4 lg:pt-8 md:relative md:py-8 md:pl-8 md:pr-16 min-w-min md:w-1/6 flex'
         className='flex-1 max-w-2xl mx-auto md:mx-0 md:max-w-sm'
       />
-      <main className='flex-1 w-full px-4 mt-40 md:mt-0 md:overflow-y-scroll md:py-8 md:px-16'>
+      <main className='flex-1 w-full px-4 mt-36 md:mt-0 md:overflow-y-scroll md:py-8 md:px-16'>
         <div className="max-w-2xl pb-12 mx-auto md:mx-0 md:max-w-7xl md:pb-18">
           <div className='space-y-universal'>{children}</div>
         </div>  
