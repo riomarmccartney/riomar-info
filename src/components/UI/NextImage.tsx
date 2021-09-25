c031ed8504e74c35e13c94a64d7aea632f310040
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-25 20:46:20 +0900

    Add loader to Image

--- src/components/UI/NextImage.tsx
+++ src/components/UI/NextImage.tsx
@@ -1,17 +1,25 @@
 /* eslint-disable jsx-a11y/alt-text */
 
-import Image, {ImageProps} from 'next/image'
+import Image, { ImageProps } from 'next/image'
 import clsx from 'clsx'
 import { useState } from 'react'
 
-export const NextImage = (props: ImageProps) => {
+export const NextImage = ({ src, ...rest }: ImageProps) => {
   const [visiblity, setVisibility] = useState(false)
+
+  const imageSrcId = src?.match(/riomar-info\/(.*?)\?/)
+  const myLoader = ({ src, width, quality }: any) => {
+    return `https://images.prismic.io/riomar-info/${src}?w=${width}&q=${quality || 75}&fm=auto`
+  }
 
   return (
     <Image 
       className={clsx(visiblity ? 'opacity-100' : 'opacity-0', 'transition-opacity duration-500 delay-300')}
+      loader={myLoader}
+      src={imageSrcId[1]}
+      layout="responsive"
       onLoad={() => setVisibility(true)} 
-      {...props}
+      {...rest}
     />
   )
 }
