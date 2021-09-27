760075834d4c0b323a524b8066bddd1a0fefb8a9
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-27 20:00:11 +0900

    Fix format

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,12 @@
 import React, { ReactElement } from 'react'
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
 
-    return { ...initialProps }
+    return initialProps
   }
-  
 
   render():ReactElement {
     return (
