aa0c44ad087d4e424dd1649f3d6eee05a530fcf6
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-09-27 20:33:31 +0900

    Add toggle

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export const Layout = ({ children, notes }: LayoutType) => {
   
   return (
     <div className='absolute flex flex-col items-stretch w-full h-full md:space-y-0 md:flex-row font-regular font-typeface tracking-bases md:space-x-16 xl:space-x-24'>
-      <Sidebar notes={notes} sidebarListVisibility={sidebarListVisibility} onMenuToggle={() => {setSidebarListVisibility(!sidebarListVisibility)}}/>
+      <Sidebar notes={notes} sidebarListVisibility={sidebarListVisibility} onMenuToggle={(bool: boolean) => {setSidebarListVisibility( bool ?? !sidebarListVisibility)}}/>
       
       <main className='w-full px-4 mt-36 md:mt-0 md:overflow-y-scroll md:py-8 md:pr-8 md:pl-0'>
         <div className='max-w-2xl pb-12 mx-auto md:mx-0 md:max-w-7xl md:pb-18'>
