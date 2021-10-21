022f36b7ca54ee5018e6b0ebd2de280996e1431a
Riomar Mccartney <3750705+riomarmccartney@users.noreply.github.com>
2021-10-21 22:29:27 +0900

    Add gtag configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,10 @@
 import '../styles/globals.css'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import * as gtag from 'utils/gtag'
 
 type NextPageWithLayout = NextPage & {
   withLayout?: (page: ReactElement, notes: any) => ReactNode
@@ -14,6 +17,19 @@ type AppPropsWithLayout = AppProps & {
 }
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   const withLayout = Component.withLayout ?? ((page) => page) 
   const { notes } = pageProps
 
