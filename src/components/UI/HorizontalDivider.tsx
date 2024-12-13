778f6b6fa72ad60f3429d5e8ada21bc44083dfce
Riomar McCartney <account@riomar.info>
2024-12-13 14:51:05 +0900

    Reset Commit

--- src/components/UI/HorizontalDivider.tsx
+++
@@ -1,5 +0,0 @@
-import clsx from 'clsx'
-
-export const HorizontalDivider = ({ className }: {className?: string}) => {
-  return <div className={className}><div className={clsx('h-px border-t border-dashed border-gray-300')}></div></div>
-}
--- src/components/slices/index.tsx
+++
@@ -1,4 +0,0 @@
-import { MultiStory } from './MultiStory'
-import { SliderStory } from './SliderStory'
-
-export { MultiStory, SliderStory }
