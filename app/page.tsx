240ea6213bf87b5a0b5f27f434f7bf36469bc6c1
Riomar McCartney <account@riomar.info>
2024-12-13 16:02:47 +0900

    New Website

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+import { Metadata } from 'next'
+import Image from 'next/image'
 
 export default function Home() {
+  const links = [
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/riomar.mccartney/',
+    },
+    {
+      name: 'Are.na',
+      url: 'https://www.are.na/riomar-mccartney',
+    },
+    {
+      name: 'Email',
+      url: 'mailto:riomar@riomar.studio',
+    },
+  ]
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="grid grid-flow-row-dense grid-cols-1 gap-4 p-2 text-sm min-h-dvh">
+      <div>
+        <h1>Riomar McCartney — マッカートニー龍馬</h1>
+        <p>Designer, Programmer, and Art Director based in Tokyo.</p>
+      </div>
+      <div>
+        <p className="max-w-prose">
+          Riomar is a was born in Japan in 1996. Growing up, he was immersed in
+          various genres and lifestyles due to his musician and artist-oriented
+          parents. This exposure to different mediums and cultures across Japan
+          and Europe allowed him to develop a unique taste for blending
+          different genres and perspectives. This ability to craft distinctive
+          directions and ideas has enabled him to stand out in his expression
+          and communication through design and direction.
+          <br />
+          <br />
+          Currently, He is working on{' '}
+          <a className="underline" href="https://upcoming.studio">
+            upcoming.studio
+          </a>
+          , a design collective that partners with artists and brands to
+          approach ideas through graphics, editorial, and art direction.
+          <br />
+          <br />
+          Links:{' '}
+          {links.map((link, index) => (
+            <>
+              <a key={link.name} className="underline" href={link.url}>
+                {link.name}
+              </a>
+              {index < links.length - 1 && ', '}
+            </>
+          ))}
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      <div className="relative row-span-3">
+        <Image
+          src="/riomar-mccartney-desk.jpg"
+          alt="Riomar's desk"
+          sizes="(max-width: 768px) 100vw, 50vw"
+          fill={true}
+          className="object-contain object-left-bottom"
+        />
+      </div>
+    </main>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'Riomar McCartney',
+  description:
+    'Riomar McCartney (マッカートニー 龍馬) is a designer, programmer, and art director based in Tokyo. He is founder of upcoming.studio',
 }
